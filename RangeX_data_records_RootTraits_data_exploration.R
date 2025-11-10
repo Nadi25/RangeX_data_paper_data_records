84ba1c99bd4375c64a99f50898f656b8c0a35dff
Nadi25 <nadine-arzt@gmx.de>
2025-11-10 17:04:48 +0100

    root traits data exploration

---
+++ RangeX_data_records_RootTraits_data_exploration.R
@@ -0,0 +1,108 @@
+
+
+## RangeX Root CHE data exploration
+
+## Data used: 
+## Date:      07.11.25
+## Author:    Nadine Arzt
+## Purpose:   Explore Root trait data 
+##            How many samples? per region? 
+
+
+# import metadata ---------------------------------------------------------
+meta_che <- read.csv("Data/Metadata/RangeX_clean_MetadataFocal_CHE.csv")
+
+
+# import root data -------------------------------------------------------------
+root_traits <- read.csv("Data/RootTraits/RangeX_clean_RootTraits_2023_CHE.csv")
+glimpse(root_traits)
+# 119 observations
+
+
+# join metadata with roots ----------------------------------------------
+roots_che <- root_traits |> 
+  left_join(meta_che, by = c("unique_plant_ID", "species"))
+
+
+# how many species --------------------------------------------------------
+species_count <- unique(root_traits$species)
+species_count
+# 9
+
+species_obs_count <- root_traits |> 
+  count(species)
+species_obs_count
+
+
+# average, sd, min, max per species and trait -------------------------------------------------------
+root_traits_summary <- root_traits |> 
+  group_by(species) |> 
+  summarise(
+    across(
+      where(is.numeric),
+      list(
+        mean = ~mean(.x, na.rm = TRUE),
+        sd   = ~sd(.x, na.rm = TRUE),
+        min  = ~min(.x, na.rm = TRUE),
+        max  = ~max(.x, na.rm = TRUE)
+      ),
+      .names = "{.col}_{.fn}"
+    )
+  )
+root_traits_summary
+
+# average, sd, min, max per species, site and biomass ----------------------------
+roots_species_site_summary <- roots_che |> 
+  group_by(site, species) |> 
+  summarise(
+    across(
+      where(is.numeric),
+      list(
+        mean = ~mean(.x, na.rm = TRUE),
+        sd   = ~sd(.x, na.rm = TRUE),
+        min  = ~min(.x, na.rm = TRUE),
+        max  = ~max(.x, na.rm = TRUE)
+      ),
+      .names = "{.col}_{.fn}"
+    )
+  )
+roots_species_site_summary
+
+
+count <- roots_che |> 
+  count(site, species)
+count
+
+# count per site ----------------------------------------------
+count2 <- roots_che |> 
+  count(site)
+count2
+
+
+# count per species ----------------------------------------------
+count3 <- roots_che |> 
+  count(species)
+count3
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
